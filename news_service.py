fbf0b43d85f652fdb6044472322616aa0630a2e4
Alif0807 <65691888+Alif0807@users.noreply.github.com>
2026-06-19 10:06:53 +0800

    Update news_service.py

--- news_service.py
+++ news_service.py
@@ -1,3 +1,9 @@
-# Economic news service
-def get_news():
-    return []
+import requests
+
+def get_mock_news():
+    return {
+        "event": "Core CPI m/m",
+        "country": "🇺🇸",
+        "impact": "🔴 High",
+        "time": "20:30 MYT"
+    }
