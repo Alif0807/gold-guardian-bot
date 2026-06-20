2dc24164fc569a3644088f96693d42efac6dbacc
Alif0807 <65691888+Alif0807@users.noreply.github.com>
2026-06-20 10:27:38 +0800

    Update economic_events.py

--- economic_events.py
+++ economic_events.py
@@ -1,19 +1,18 @@
+from forexfactory_service import fetch_calendar
+
 def get_events():
 
+    soup = fetch_calendar()
+
+    if soup is None:
+        return []
+
+    # temporary test data
     return [
-
         {
-            "name":"Core CPI m/m",
-            "country":"🇺🇸",
-            "impact":"🔴",
-            "time":"20:30 MYT"
-        },
-
-        {
-            "name":"FOMC Statement",
-            "country":"🇺🇸",
-            "impact":"🔴",
-            "time":"02:00 MYT"
+            "name": "Core CPI m/m",
+            "country": "🇺🇸",
+            "impact": "🔴",
+            "time": "20:30 MYT"
         }
-
     ]
