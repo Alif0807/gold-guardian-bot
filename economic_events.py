be95de0759cad6b35fdea66443805ef9f3b92f14
Alif0807 <65691888+Alif0807@users.noreply.github.com>
2026-06-20 10:19:54 +0800

    Create economic_events.py

---
+++ economic_events.py
@@ -0,0 +1,19 @@
+def get_events():
+
+    return [
+
+        {
+            "name":"Core CPI m/m",
+            "country":"🇺🇸",
+            "impact":"🔴",
+            "time":"20:30 MYT"
+        },
+
+        {
+            "name":"FOMC Statement",
+            "country":"🇺🇸",
+            "impact":"🔴",
+            "time":"02:00 MYT"
+        }
+
+    ]
