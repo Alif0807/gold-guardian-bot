0dd278bf14617a16779ae0eff224e85977f9b949
Alif0807 <65691888+Alif0807@users.noreply.github.com>
2026-06-20 10:28:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from daily_summary import generate_summary
-from scheduler_service import should_send_daily_summary
 from telegram_service import send_message
 
-if should_send_daily_summary():
+message = generate_summary()
 
-    message = generate_summary()
-
-    send_message(message)
+send_message(message)
 
 print("Done")
